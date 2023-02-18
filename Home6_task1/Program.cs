fb3af951fea504561d29bb77f9aed8fb8724ac07
Alex TK <lumeneti@gmail.com>
2023-02-18 11:47:53 +0300

    Шестое ДЗ

---
+++ Home6_task1/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] CreatArray(string massage)
+{
+    Console.WriteLine(massage);
+    int sizeMas = Convert.ToInt32(Console.ReadLine());
+    int[] mas = new int[sizeMas];
+    return mas;
+}
+
+int[] FilArray(int[] baseArray, string massage)
+{
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        Console.WriteLine(massage);
+        baseArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return baseArray;
+}
+
+void DisplayElements(int[] baseArray)
+{
+    Console.WriteLine("Введёные числа");
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        Console.WriteLine($"{baseArray[i]}");
+    }
+}
+
+void FindNum(int[] baseArray)
+{
+    int count = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Колличество чисел со значением больше нуля{count}");
+    Console.ResetColor();
+}
+
+int[] baseArray = CreatArray("Введите колличество чисел");
+string massage = $"Введите число";
+FilArray(baseArray, massage);
+DisplayElements(baseArray);
+FindNum(baseArray);
---
+++ Home6_task2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+const int K1 = 0;
+const int B1 = 1;
+const int K2 = 2;
+const int B2 = 3;
+
+void UserInpunt(double[] coeff, string massage)
+{
+    Console.WriteLine($"Введите коэфициенты");
+    for (int i = 0; i <= coeff.Length - 1; i++)
+    {
+        coeff[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+double FindX (double[] coeff)
+{
+    double x = (coeff[B2]-coeff[B1])/(coeff[K1]-coeff[K2]);
+    return x;
+}
+
+double FindY(double[] coeff)
+{
+    double x = FindX(coeff);
+    double y = coeff[K1]*x + coeff[B1];
+    return y;
+}
+
+double[] coeff = new double[4];
+
+UserInpunt(coeff, "Введите коэфициенты для рассчета");
+double resultX = FindX(coeff);
+double resultY = FindY(coeff);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Координаты пересечения двух прямых {resultX}, {resultY}");
+Console.ResetColor();
