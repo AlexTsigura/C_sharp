d32f6608298bfd806115fb554cfe9e9bb58f9079
Alex TK <lumeneti@gmail.com>
2023-02-26 19:25:33 +0300

    Итоговый проект

---
+++ Final_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputInt(string msg)
+{
+    System.Console.Write(msg + " -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Check (int min, int max)
+{
+    if (max < 1 || min > max)
+    {
+        System.Console.WriteLine("С такими числами дальнейшие действия невозможны");
+        Environment.Exit(1);
+    }
+}
+void ShowEven(int min, int max)
+{
+    if (min % 2 == 1) min += 1;
+    if (min <= 0) min = 2;
+    if (min > max) return;
+    System.Console.Write($"{min}; ");
+    ShowEven(min + 2, max);   
+}
+int little = InputInt("Введите, от какого числа начать");
+int big = InputInt("Введите, до какого числа подняться");
+Check(little, big);
+ShowEven(little, big);
---
+++ Final_task3/Program.cs
@@ -0,0 +1,24 @@
+﻿int InputInt(string msg)
+{
+    System.Console.Write(msg + " -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Check (int num1, int num2)
+{
+    if (num1 < 0 || num2 < 0)
+    {
+        System.Console.WriteLine("С такими числами дальнейшие вычисления невозможны.");
+        Environment.Exit(1);
+    }
+}
+int AckermanFunc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermanFunc(m - 1, 1);
+    return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
+}
+int argumentM = InputInt("Введите аргумент m");
+int argumentN = InputInt("Введите аргумент n");
+Check(argumentM, argumentN);
+int result = AckermanFunc(argumentM, argumentN);
+System.Console.WriteLine($"Результат вычисления функции Аккермана F(m,n) = {result}");
